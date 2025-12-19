79ae6e3094417d0810607ca5a76d03f9ac1be65e
Noah Seymour <ns2224@ic.ac.uk>
2025-12-19 22:10:37 +0000

    refactor(HttpParser): re-wrote tchar parser.

--- src/main/scala/Server/HttpParser.scala
+++ src/main/scala/Server/HttpParser.scala
@@ -4,6 +4,7 @@ import ParserCombinator._
 
 object HttpParser {
   def singleSpace: Parser[Char] = sat(_ == ' ')
-  def tchar: Parser[Char] = ???
+  def tchar: Parser[Char] = sat(_.isLetterOrDigit) + sat("!#$%&'*+-.^_`|~".contains(_))
+  
   def token: Parser[String] = ???
 }
